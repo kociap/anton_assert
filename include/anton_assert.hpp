ebe0e00dcc63919255d1aea82dbcc4f5aaec6079
Piotr Kocia <p.kocia@wp.pl>
2019-09-07 19:56:35 +0200

    Renamed assert macro

--- include/anton_assert.hpp
+++ include/anton_assert.hpp
@@ -4,9 +4,9 @@
 void anton_assert(char const* message, char const* file, unsigned long long line);
 
 #ifndef NDEBUG
-#    define ANTONIO_ASSERT(condition, msg) (static_cast<bool>(condition) ? (void)0 : anton_assert(msg, __FILE__, __LINE__))
+#    define ANTON_ASSERT(condition, msg) (static_cast<bool>(condition) ? (void)0 : anton_assert(msg, __FILE__, __LINE__))
 #else
-#    define ANTONIO_ASSERT(condition, msg) ((void)0)
+#    define ANTON_ASSERT(condition, msg) ((void)0)
 #endif
 
 #endif // !ANTON_ASSERT_LIB_ANTON_ASSERT_HPP_INCLUDE
